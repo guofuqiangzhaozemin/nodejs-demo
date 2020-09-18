820f0a37c543b0dae5d15165319340f98f85ee11
zhaozemin <1963765653@qq.com>
2020-09-18 17:48:53 +0800

    02-time.js demo code ok

---
+++ 03-console/01-format.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const user={
+  name:'zhaozemin',
+  age:20,
+  qq:'1111111',
+};
+const log=console.log;
+
+log('name:%s',user.name);
+log('age:%d',user.age);
+log('user:%j',user);
+log('qq:%s',user.qq);
+log('qq:',user.qq);
+log('qq:'+user.qq);
+log(`qq:${user.qq}`);
