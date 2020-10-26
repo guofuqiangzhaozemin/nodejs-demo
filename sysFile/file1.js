04c5e12c34b5390f5c3c51dd4a6fac451909edc6
赵泽敏 <1963765653@qq.com>
2020-10-26 08:19:25 +0800

    zzm

---
+++ sysFile/file1.js
@@ -0,0 +1,74 @@
+var fs = require("fs");
+function writeFileContent(content) {
+	var tag = fs.existsSync("./log.txt");
+	if(tag) {
+		var result = fs.readFileSync("./log.txt");
+		fs.writeFileSync("./log.txt", result + "\r\n" + content);
+	}
+	else {
+		fs.writeFileSync("./log.txt", content);
+	}
+}
+writeFileContent(title);
+
+var fs = require("fs");
+function writeFileContent(content) {
+	var tag = fs.existsSync("./log.txt");
+	if(tag) {
+		var result = fs.readFileSync("./log.txt");
+		fs.writeFileSync("./log.txt", result + "\r\n" + content);
+	}
+	else {
+		fs.writeFileSync("./log.txt", content);
+	}
+}
+writeFileContent(title);
+
+var fs = require("fs");
+function writeFileContent(content) {
+	var tag = fs.existsSync("./log.txt");
+	if(tag) {
+		var result = fs.readFileSync("./log.txt");
+		fs.writeFileSync("./log.txt", result + "\r\n" + content);
+	}
+	else {
+		fs.writeFileSync("./log.txt", content);
+	}
+}
+writeFileContent(title);
+var fs = require("fs");
+function writeFileContent(content) {
+	var tag = fs.existsSync("./log.txt");
+	if(tag) {
+		var result = fs.readFileSync("./log.txt");
+		fs.writeFileSync("./log.txt", result + "\r\n" + content);
+	}
+	else {
+		fs.writeFileSync("./log.txt", content);
+	}
+}
+writeFileContent(title);
+var fs = require("fs");
+function writeFileContent(content) {
+	var tag = fs.existsSync("./log.txt");
+	if(tag) {
+		var result = fs.readFileSync("./log.txt");
+		fs.writeFileSync("./log.txt", result + "\r\n" + content);
+	}
+	else {
+		fs.writeFileSync("./log.txt", content);
+	}
+}
+writeFileContent(title);
+var fs = require("fs");
+function writeFileContent(content) {
+	var tag = fs.existsSync("./log.txt");
+	if(tag) {
+		var result = fs.readFileSync("./log.txt");
+		fs.writeFileSync("./log.txt", result + "\r\n" + content);
+	}
+	else {
+		fs.writeFileSync("./log.txt", content);
+	}
+}
+writeFileContent(title);
